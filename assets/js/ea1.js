781ea0ffc687faa79a31201d3556687eb19272c4
kotodorova <76124315+kotodorova@users.noreply.github.com>
2022-10-15 23:14:54 +0300

    Add files via upload

---
+++ assets/js/ea1.js
@@ -0,0 +1,58 @@
+const images = [
+    "images/01.png",
+    "images/02.png",
+    "images/03.png",
+    "images/04.png",
+    "images/05.png",
+    "images/06.png",
+    "images/07.png",
+  ];
+
+  var index = 0;
+    // function drawImage() {
+    //   var img = document.createElement("img");
+    //   img.src = images[index];
+    //   document.getElementById("content").appendChild(img);
+    // }
+
+    // 76 = 'l' key
+    document.addEventListener("keydown", function (event) {
+      if (event.keyCode == 82) {
+        if (index == 0) {
+          index = 6;
+          console.log(index);
+        } else {
+          index--;
+          console.log(index);
+        }
+        document.getElementById("images").src = images[index];
+        console.log("L.");
+      }
+    });
+
+    // 82 = 'r' key
+    document.addEventListener("keydown", function (event) {
+      if (event.keyCode ==76) {
+        if (index == 6) {
+          index = 0;
+          console.log(index);
+        } else {
+          index++;
+          console.log(index);
+        }
+        document.getElementById("images").src = images[index];
+        console.log("R.");
+      }
+    });
+
+    document.addEventListener("keydown", function (event) {
+        if (event.keyCode ==97) {
+            index++;
+            console.log(index);
+          }
+          document.getElementById("images").src = images[index];
+          console.log("R.");
+        }
+      );
+
+    
