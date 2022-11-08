7508832fcc8bc953c58bc8729914590380e762ae
kotodorova <76124315+kotodorova@users.noreply.github.com>
2022-11-08 14:31:00 +0200

    Add files via upload

--- assets/js/ea3.js
+++ assets/js/ea3.js
@@ -81,13 +81,13 @@ var colors = [0.196,0.133,0.035, //1
      // Create an empty buffer object and store vertex data
      var vertex_buffer = gl.createBuffer();
      gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
-     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
+     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
      gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
      // Create an empty buffer object and store Index data
      var Index_Buffer = gl.createBuffer();
      gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer);
-     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.DYNAMIC_DRAW);
+     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
      gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
 
      // Create an empty buffer object and store color data
